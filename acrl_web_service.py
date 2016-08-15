be1b787a24681cdd2dac363617086c40f3ec60ab
Teague Algie <talgie@cleversafe.com>
2016-08-15 08:07:24 -0500

    some check-in config automagic awesome, incomplete

--- acrl_web_service.py
+++ acrl_web_service.py
@@ -1,6 +1,7 @@
 '''
 Bottle server with api methods for starting everything
 '''
+# TODO: make templates and render some html.
 from bottle import Bottle, run, request
 import subprocess
 import os
@@ -9,7 +10,7 @@ import os
 SERVER_PATH = 'C:\\acrl\\'
 CONFIG_PATH = os.path.join(SERVER_PATH, 'presets')
 AC_SERVER_EXE = 'acServer.exe'
-
+ENTRY_LIST = 'entry_list.ini'
 
 # HTTP Verbs
 POST = "POST"
@@ -38,7 +39,7 @@ def status():
         acrl_status = "{}The race server is not running.\n".format(acrl_status)
     return acrl_status
 
-
+# TODO: all these returns will have to be redone using templates
 @acrl.route('/upload', method=POST)
 def upload_configs():
     upload = request.files.get('server_cfg')
@@ -50,8 +51,8 @@ def upload_configs():
     server_config_dir = output[0]
     next_server_config_dir = "{}{}".format(server_config_dir[:-2], int(server_config_dir[-2:])+1)
 
-    if not os.path.exists(next_server_config_dir):
-        os.makedirs(next_server_config_dir)
+    if not os.path.exists(os.path.join(CONFIG_PATH, next_server_config_dir)):
+        os.makedirs(os.path.join(CONFIG_PATH, next_server_config_dir))
 
     file_path = os.path.join(CONFIG_PATH, next_server_config_dir, upload.filename)
     upload.save(file_path)
@@ -76,6 +77,30 @@ def server_running():
         server_is_running = True
     return server_is_running
 
+
+# Returns new entry list as a string
+# TODO: Implement
+def current_entry_list(checkin_url):
+    # Get the check-in list from google sheets
+    checkin_list = []
+
+    # Get the full entry list of all members (also store as a google doc?)
+    racers = {'name': 'entry_string'}
+
+    entries = []
+    for checked_in in checkin_list:
+        entries.append(racers[checked_in])
+
+    return "\n\n".join(entries)
+
+
+# Writes unsafely to the current config directory
+def write_current_entry_list(entry_list_string):
+    p1 = subprocess.Popen(["cmd", "/C", "DIR /B", CONFIG_PATH], stdout=subprocess.PIPE)
+    output = sorted(p1.communicate()[0])
+    server_config_dir_name = output[0]
+    with open(os.path.join(CONFIG_PATH, server_config_dir_name, ENTRY_LIST), 'w') as entry_list_ini:
+        entry_list_ini.write(entry_list_string)
 
 if __name__ == "__main__":
     run(acrl, host='127.0.0.1', port=8080)
